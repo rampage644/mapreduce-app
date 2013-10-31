607c02f530d7df7498410a41d67c3ddb93819046
Sergei Turukin <rampage644@gmail.com>
2013-10-31 15:57:30 +0200

    working sample with links to output files

--- map.py
+++ map.py
@@ -5,16 +5,13 @@ import zmapreduce
 
 import mapreduce
 
-
-node_id = int(sys.argv[1].split('-')[1])
-print sys.argv
-print "map %d" % node_id
+print >> sys.stderr, sys.argv, sys.executable, sys.argv0
+node_id = int(sys.argv0.split('-')[1])
 
 m = zmapreduce.Mapper(node_id)
 m.map_fn = mapreduce.Map
 m.reduce_fn = mapreduce.Reduce
 m.combine_fn = mapreduce.Combine
-m.comparator_fn = mapreduce.ComparatorHash
 m.mritem_size = 28
 m.hash_size = 10
 m.start()
--- reduce.py
+++ reduce.py
@@ -5,14 +5,13 @@ import zmapreduce
 
 import mapreduce
 
-node_id = int(sys.argv[1].split('-')[1])
-print sys.argv
-print "reduce %d" % node_id
+print >> sys.stderr, sys.argv, sys.executable, sys.argv0
+node_id = int(sys.argv0.split('-')[1])
+
 r = zmapreduce.Reducer(node_id)
 r.map_fn = mapreduce.Map
 r.reduce_fn = mapreduce.Reduce
 r.combine_fn = mapreduce.Combine
-r.comparator_fn = mapreduce.ComparatorHash
 r.mritem_size = 28
 r.hash_size = 10
 r.start()
